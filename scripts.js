3919b6bff389b6f31b82b438f4cee9dfb5ce660b
limaluca <lucaslimamesmo@gmail.com>
2020-10-06 15:44:42 -0300

    Replacing data to be shown on a modal

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const modalOverlay = document.querySelector('.modalOverlay');
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener('click', function() {
+        modalOverlay.classList.add('active');
+
+
+        //   Getting the contents on the selectors of each card
+        const dishImageId = card.getAttribute('id');
+        const dishName = card.querySelector('h4').innerHTML;
+        const dishAuthor = card.querySelector('p').innerHTML;
+
+
+        //   Replacing the content for the selected card 
+        modalOverlay.querySelector('img').src = `/layouts/assets/${dishImageId}`
+        modalOverlay.querySelector('h4').innerHTML = `${dishName}`;
+        modalOverlay.querySelector('p').innerHTML = `${dishAuthor}`;
+    })
+}
+
+document.querySelector('.closeModal').addEventListener('click', function() {
+    modalOverlay.classList.remove('active');
+})
