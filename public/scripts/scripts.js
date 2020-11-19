45d3801c9ac8e54486aec766f9725f303a099ade
limaluca <lucaslimamesmo@gmail.com>
2020-11-19 17:34:32 -0300

    Adding the hide and show button

---
+++ public/scripts/scripts.js
@@ -0,0 +1,48 @@
+const modalOverlay = document.querySelector('.modalOverlay');
+const cards = document.querySelectorAll('.card')
+const hideButton = document.querySelectorAll('.hide-show')
+
+for (let card of cards) {
+    card.addEventListener('click', function() {
+        modalOverlay.classList.add('active');
+
+
+        //   Getting the contents on the selectors of each card
+        const recipeImageId = card.getAttribute('id');
+        const recipeName = card.querySelector('h4').innerHTML;
+        const recipeAuthor = card.querySelector('p').innerHTML;
+
+
+        //   Replacing the content for the selected card 
+        modalOverlay.querySelector('img').src = `/layouts/assets/${recipeImageId}`
+        modalOverlay.querySelector('h4').innerHTML = `${recipeName}`;
+        modalOverlay.querySelector('p').innerHTML = `${recipeAuthor}`;
+
+    })
+}
+
+
+
+
+const buttons = document.querySelectorAll('.button')
+const infos = document.querySelectorAll('.recipe__info')
+
+for (let button of buttons) {
+    button.addEventListener('click', function() {
+        if (button.innerHTML === 'ESCONDER') {
+            button.innerHTML = 'MOSTRAR'
+        } else {
+            button.innerHTML = 'ESCONDER'
+        }
+    })
+}
+
+for (const button in buttons) {
+    buttons[button].addEventListener('click', function() {
+        if (infos[button].classList.contains('hide')) {
+            infos[button].classList.remove('hide')
+        } else {
+            infos[button].classList.add('hide')
+        }
+    })
+}
